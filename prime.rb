d7507c395b6f4aee2901ddb93650d1c7713abf2d
Nerly Ton <nerlyton@gmail.com>
2020-07-31 22:04:42 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -7,17 +7,4 @@ def prime?(n)
   end
 end
 
-# def prime?(n)
-#   if n < 2
-#     return false
-#   elsif n <= 3
-#     return true
-#   else (2..n/2).none? do |x|
-#     n % x == 0
-#   end
-#   end
-# end
-
-
-# interger % interger
 
