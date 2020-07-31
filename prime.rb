f259b8596de926801cb7299c1a41988768ef3558
Nerly Ton <nerlyton@gmail.com>
2020-07-31 22:09:26 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,17 +1,24 @@
-def prime?(n)
-  # range_to_array = (0..max).to_a 
-  if n < 2 
-    false
-  else (2..n/2).none? {|num|n % num == 0}
-    true
-  end
-end
+# def prime?(n)
+#   # range_to_array = (0..max).to_a 
+#   if n < 2 
+#     false
+#   else (2..n/2).none? {|num|n % num == 0}
+#     true
+#   end
+# end
 
-def prime?(n)
-  # range_to_array = (0..max).to_a 
-  if (2..n/2).none? {|num|n % num == 0}
-    true
-  else 
-    false
-  end
+# def prime?(n)
+#   # range_to_array = (0..max).to_a 
+#   if (2..n/2).none? {|num|n % num == 0}
+#     true
+#   else 
+#     false
+#   end
+# end
+
+
+def prime(n)
+  return false if n < 2
+
+  (2..n/2).none?{|i| n % i == 0}
 end
