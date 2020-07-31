b73d0df757d1b11abe7905884cf385e0d6f7ad04
Nerly Ton <nerlyton@gmail.com>
2020-07-31 21:57:31 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -7,14 +7,16 @@
 #   end
 # end
 
-def prime(n)
- puts "That's not an integer." unless n.is_a? Integer
-  is_prime = true
-  for i in 2..n-1
-    if n % i == 0
-      is_prime = false
-    end
+def prime?(n)
+  if n <= 1
+    return false
+  elsif n <= 3
+    return true
+  else (2..n/2).none? do |x|
+    n % x == 0
   end
+  end
+end
 
 
 # interger % interger
