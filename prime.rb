2f42480a03c5f42dd3ded55b90e3d541b8aeb794
Nerly Ton <nerlyton@gmail.com>
2020-07-31 21:56:32 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,20 @@
-def prime?(n)
-  # range_to_array = (0..max).to_a 
-  if (2..n-0).none? {|num|n % num == 0}
-    true 
-  else
-    false
+# def prime?(n)
+#   # range_to_array = (0..max).to_a 
+#   if (2..n-0).none? {|num|n % num == 0}
+#     true 
+#   else
+#     false
+#   end
+# end
+
+def prime(n)
+ puts "That's not an integer." unless n.is_a? Integer
+  is_prime = true
+  for i in 2..n-1
+    if n % i == 0
+      is_prime = false
+    end
   end
-end
-
-
 
 
 # interger % interger
