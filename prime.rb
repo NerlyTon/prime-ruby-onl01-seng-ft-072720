f5064334ea4ef97243c3c8f3975e9f723263e043
Nerly Ton <nerlyton@gmail.com>
2020-07-31 21:47:39 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,10 @@
 def prime?(number)
   # range_to_array = (0..max).to_a 
-  (2..number-1).none? {|num|number % num == 0}
+  if (2..number-1).none? {|num|number % num == 0}
+    true 
+  else
+    false
+  end
 end
 
 
